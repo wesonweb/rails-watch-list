9a6bdec1aeb7edddcbcd595f6ffd3b8477d2daef
Wes Mehaffy <wesonweb@gmail.com>
2023-01-30 10:30:26 +0000

    seed file from movies API

--- Gemfile
+++ Gemfile
@@ -21,6 +21,9 @@ gem "jsbundling-rails"
 # Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
 gem "turbo-rails"
 
+# rest-client
+gem "rest-client"
+
 # Hotwire's modest JavaScript framework [https://stimulus.hotwired.dev]
 gem "stimulus-rails"
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'rest-client'
+puts 'Getting movies from API...'
+def fetch_movies
+  movies = RestClient.get 'https://tmdb.lewagon.com/movie/top_rated'
+  movies_array = JSON.parse(movies)['results']
+  movies_array.each do |movie|
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: movie['poster_path'],
+      rating: movie['vote_average']
+    )
+  end
+end
+
+fetch_movies
+puts 'Seeding done!'
