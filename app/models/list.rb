9fde248e219f72c8a201c92265d20b28ed7d60fe
Wes Mehaffy <wesonweb@gmail.com>
2023-02-02 16:11:15 +0000

    add cloudinary image upload for list index and show views

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,7 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks, dependent: :destroy
+  has_one_attached :photo
+  validates :photo, presence: true
   validates :name, presence: true, uniqueness: true # A list must have a unique name.
 end
